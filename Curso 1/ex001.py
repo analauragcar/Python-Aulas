0566bad61534e4e62614c11b15f13bb1e25ab310
Ana Laura Gomes <148863673+analauragcar@users.noreply.github.com>
2024-08-06 09:56:32 -0300

    atualizei

---
+++ Curso 1/ex001.py
@@ -0,0 +1,4 @@
+nome = ("Laura")
+idade = input("Qual a sua idade?")
+ano = 2024 - int(idade)
+print(f"Olá, {nome}, você nasceu em {ano} ")
--- Curso 1/index.py
+++
@@ -1,4 +0,0 @@
-idade = 17
-nome = 'Laura'
-
-print(f"Olá, {nome}, você tem {idade} anos")
